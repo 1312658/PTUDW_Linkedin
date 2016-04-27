48ee8866a0a12572fd7aefc06c5880c3c6fa5154
1312658 <leanhtuan2802it@gmail.com>
2016-04-27 14:26:32 +0700

    Code tay lai giao dien!

---
+++ GiuaKiLan01/js/myscript.js
@@ -0,0 +1,106 @@
+
+angular.module('beTapTinh', [])
+
+.controller('mayTinh', function($scope) {
+    $scope.ketqua = "" ;
+    $scope.phepTinh = false;
+
+    $scope.tinh = function()
+    {
+        if ($scope.checkInput() == false) {
+            $scope.ketqua = 0;
+            return;
+        }
+
+        $scope.thongBao = "Không có thông báo!!";
+        var so1 = Number($scope.So1);
+        var so2 = Number($scope.So2);
+        var kq;
+
+        var phepTinh = $scope.phepTinh;
+        if(phepTinh == "Cong")
+        {
+            kq = so1 + so2;
+            $scope.ketqua = kq;
+        }
+        else if(phepTinh == "Tru")
+        {
+            kq = so1 - so2;
+            $scope.ketqua = kq;
+        }
+			
+        else if(phepTinh == "Nhan")
+        {
+            kq = so1 * so2;
+            $scope.ketqua = kq;
+        }
+        else if(phepTinh == "Chia")
+        {
+            kq = so1 / so2;
+            $scope.ketqua = kq;
+        }
+    }
+
+    $scope.reset = function()
+    {
+        $scope.So1 = null;
+        $scope.So2 = null;
+        $scope.thongBao = "";
+        $scope.ketqua = "";
+        $scope.phepTinh = false;
+    }
+
+    $scope.checkInput = function()
+    {
+        var a = $scope.So1;
+        var b = $scope.So2;
+
+        if (a == null || b == null) {
+            $scope.thongBao = "Không có thông báo!!";
+            return false;
+        }
+
+        else if (a == "" || b == "") {
+            $scope.thongBao = "Chưa nhập đủ cả 2 số!!";
+            return false;
+        }
+
+        else if ($scope.phepTinh == false) {
+            $scope.thongBao = "Chưa chọn một phép tính!";
+            return false;
+        }
+
+        
+        else if (!Number(a))
+        {
+            if (a != 0) {
+                $scope.thongBao = "Đầu vào không phải là số!";
+                return false;
+            }
+        }
+
+        if (!Number(b)) {
+            if (b != 0) {
+                $scope.thongBao = "Đầu vào không phải là số!";
+                return false;
+            }
+            else 
+            {
+                if ($scope.phepTinh == "Chia") {
+                        $scope.thongBao = "Không thể chia cho 0!";
+                        return false;
+                    }
+                    else {
+                        return true;
+                    }
+            }
+        }
+
+        else {
+            return true;
+        }
+    }
+});
+
+
+
